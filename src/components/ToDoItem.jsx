94d2ace2ff64f77840f12ae9304139a00fb7ae0c
Hector Lopez <hlopezcarballo@gmail.com>
2021-10-29 00:29:00 -0500

    Add stylings for checked state

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 
 import { ListItemStyle, CheckButton } from "../styles/ListItemStyles";
 
 import { ReactComponent as CrossIcon } from "../assets/icon-cross.svg";
 
+const StyledSpan = styled.span`
+  text-decoration: ${(props) => (props.checked ? "line-through" : "none")};
+  color: ${(props) =>
+    props.checked ? props.theme.fontColors.grayed : "inherit"};
+`;
+
 const ToDoItem = () => {
   return (
     <ListItemStyle>
       <CheckButton />
-      <span>Complete online JavaScript course</span>
+      <StyledSpan>Complete online JavaScript course</StyledSpan>
       <CrossIcon />
     </ListItemStyle>
   );
